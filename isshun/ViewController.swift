f65979adc8971772ba93c71b7b8b396cb35d0ed2
金森幹斗 <mikito1188@icloud.com>
2015-05-10 16:24:42 +0900

    Initial commit

--- isshun/ViewController.swift
+++ isshun/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var label:UILabel!
+    
+    var timer:NSTimer!
+    
+    var count:Double = 0.0
+    
+    @IBOutlet var button:UIButton!
+    
+    @IBOutlet var oneMore:UIButton!
+    
+    @IBOutlet var timeLab:UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        start()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func a(){
+        
+        label.text = "😆"
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.001, target: self, selector: Selector("b"), userInfo: nil, repeats: true)
+        
+        button.hidden = false
+        
+        
+    }
+    func b(){
+        
+        count += 0.001
+        
+    }
+    func start(){
+        
+        button.hidden = true
+        
+        oneMore.hidden = true
+        
+        label.text = nil
+        
+        var count:Int = Int(arc4random_uniform(4))
+        count += 3
+        
+        NSTimer.scheduledTimerWithTimeInterval(Double(count), target: self, selector: Selector("a"), userInfo: nil, repeats: false)
+        
+        timeLab.text = nil
+        
+    }
+    
+    @IBAction func Tap(){
+        
+        timer.invalidate()
+        
+        timeLab.text = String(format: "%.3  f", Float(count))
+        
+        oneMore.hidden = false
+        
+    }
 
+    @IBAction func tapMokkai(){
+        
+        start()
+        
+    }
 }
 
